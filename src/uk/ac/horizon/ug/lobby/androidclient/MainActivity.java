a3664ccdaec3083adde70f71bdbf72214cd18ee4
Chris Greenhalgh <chris.greenhalgh@nottingham.ac.uk>
2010-07-05 16:16:18 +0100

    initial empty project from eclipse

---
+++ src/uk/ac/horizon/ug/lobby/androidclient/MainActivity.java
@@ -0,0 +1,13 @@
+package uk.ac.horizon.ug.lobby.androidclient;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
